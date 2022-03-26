525030ad79f39c42b51a94c04b7767bd2fe675ff
matt-hogan <mrhogan103@gmail.com>
2022-03-25 19:25:36 -0500

    Update Database Credentials

--- model/database.php
+++ model/database.php
@@ -1,5 +1,5 @@
 <?php
-    $dsn = "mysql:host=	acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=i80o0p3vudg01ue9";
+    $dsn = "mysql:host=acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=i80o0p3vudg01ue9";
     $username = "sv2j447g56es8odf";
     $password = "td5fr9r22cehoad2";
 
